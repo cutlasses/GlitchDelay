08007fee78f8b7ae0e0237809bf823067c9f884a
Cutlasses <spitkethly@gmail.com>
2016-09-25 13:56:44 +0100

    Starting to implement Glitch Delay buffer (not used currently)

--- GlitchDelayEffect.h
+++ GlitchDelayEffect.h
@@ -1,31 +1,19 @@
-/*
 #pragma once
 
 #include <AudioStream.h>
 
-#define FREEZE_QUEUE_SIZE_IN_BYTES     1024*50      // 50k
+#define DELAY_BUFFER_SIZE_IN_BYTES     1024*50      // 50k
 
 
 class AUDIO_FREEZE_EFFECT : public AudioStream
 {
-  byte                  m_buffer[FREEZE_QUEUE_SIZE_IN_BYTES];
+  byte                  m_buffer[DELAY_BUFFER_SIZE_IN_BYTES];
   audio_block_t*        m_input_queue_array[1];
   
   float                 m_head;     // read head when audio is frozen, write head when not frozen
 
-  float                 m_speed;
-
-  int                   m_loop_start;   // index of the first sample to play in loop
-  int                   m_loop_end;     // index of the last sample to play in loop
-
   int                   m_sample_size_in_bits;
   int                   m_buffer_size_in_samples;
-
-  bool                  m_freeze_active;
-  bool                  m_reverse;
-  
-  bool                  m_one_shot;     // play the section once rather than loop
-  bool                  m_paused;
 
   // store 'next' values, otherwise interrupt could be called during calculation of values
   float                 m_next_sample_size_in_bits;
@@ -47,10 +35,6 @@ class AUDIO_FREEZE_EFFECT : public AudioStream
   void                  advance_head( float inc );
 
   void                  set_bit_depth_impl( int sample_size_in_bits );
-  void                  set_length_impl( float length );
-  void                  set_centre_impl( float centre );
-  void                  set_speed_impl( float speed );
-
   
 public:
 
@@ -58,14 +42,7 @@ public:
 
   virtual void          update();
 
-  bool                  is_freeze_active() const;
-  
-  void                  set_freeze( bool active );
-  void                  set_length( float length );
-  void                  set_centre( float centre );
-  void                  set_speed( float speed );
-  void                  set_reverse( bool reverse );
   void                  set_bit_depth( int sample_size_in_bits );
 };
-*/
+
 
--- GlitchDelayEffect.ino
+++ GlitchDelayEffect.ino
@@ -1,4 +1,3 @@
-/*
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -15,10 +14,10 @@ const float MAX_SPEED( 4.0f );
 
 /////////////////////////////////////////////////////////////////////
 
-int freeze_queue_size_in_samples( int sample_size_in_bits )
+int delay_buffer_size_in_samples( int sample_size_in_bits )
 {
   const int bytes_per_sample = sample_size_in_bits / 8;
-  return FREEZE_QUEUE_SIZE_IN_BYTES / bytes_per_sample;
+  return DELAY_BUFFER_SIZE_IN_BYTES / bytes_per_sample;
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -28,23 +27,14 @@ AUDIO_FREEZE_EFFECT::AUDIO_FREEZE_EFFECT() :
   m_buffer(),
   m_input_queue_array(),
   m_head(0),
-  m_speed(1.0f),
-  m_loop_start(0),
-  m_loop_end(freeze_queue_size_in_samples(16) - 1),
   m_sample_size_in_bits(16),
-  m_buffer_size_in_samples( freeze_queue_size_in_samples( 16 ) ),
-  m_freeze_active(false),
-  m_reverse(false),
-  m_one_shot( false ),
-  m_paused( false ),
-  m_next_sample_size_in_bits(16),
-  m_next_length(1.0f),
-  m_next_centre(0.5f),
-  m_next_speed(1.0f)
+  m_buffer_size_in_samples( delay_buffer_size_in_samples( 16 ) ),
+  m_next_sample_size_in_bits(16)
 {
   memset( m_buffer, 0, sizeof(m_buffer) );
 }
 
+/*
 int AUDIO_FREEZE_EFFECT::wrap_index_to_loop_section( int index ) const
 {
   if( index > m_loop_end )
@@ -60,6 +50,7 @@ int AUDIO_FREEZE_EFFECT::wrap_index_to_loop_section( int index ) const
     return index;
   }
 }
+*/
 
 void AUDIO_FREEZE_EFFECT::write_sample( int16_t sample, int index )
 {
@@ -134,13 +125,14 @@ void AUDIO_FREEZE_EFFECT::read_from_buffer( int16_t* dest, int size )
     dest[x]                   = read_sample( trunc_to_int(m_head) );
     
     // head will have limited movement in freeze mode
-    if( ++m_head >= m_loop_end )
+    if( ++m_head >= m_buffer_size_in_samples )
     {
-      m_head                  = m_loop_start;
+      m_head                  = 0;
     } 
   }
 }
 
+/*
 void AUDIO_FREEZE_EFFECT::read_from_buffer_with_speed( int16_t* dest, int size )
 {          
     ASSERT_MSG( trunc_to_int(m_head) >= 0 && trunc_to_int(m_head) < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::read_from_buffer_with_speed()" );
@@ -184,12 +176,13 @@ void AUDIO_FREEZE_EFFECT::read_from_buffer_with_speed( int16_t* dest, int size )
       }
     }
 }
+*/
 
 void AUDIO_FREEZE_EFFECT::advance_head( float inc )
 {
   // advance the read head - clamped between start and end
   // will read backwards when in reverse mode
-
+/* TODO!!!!
   bool end_reached    = false;
 
   if( m_loop_start == m_loop_end )
@@ -254,19 +247,27 @@ void AUDIO_FREEZE_EFFECT::advance_head( float inc )
   {
     m_paused = true;
   }
+*/
 }
 
 void AUDIO_FREEZE_EFFECT::update()
 {      
   set_bit_depth_impl( m_next_sample_size_in_bits);
-  set_length_impl( m_next_length );
-  set_centre_impl( m_next_centre );
-  set_speed_impl( m_next_speed );
 
   ASSERT_MSG( trunc_to_int(m_head) >= 0 && trunc_to_int(m_head) < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::update()" );
-  ASSERT_MSG( m_loop_start >= 0 && m_loop_start < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::update()" );
-  ASSERT_MSG( m_loop_end >= 0 && m_loop_end < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::update()" );
-  
+
+  audio_block_t* block        = receiveReadOnly();
+
+  if( block != nullptr )
+  {
+    write_to_buffer( block->data, AUDIO_BLOCK_SAMPLES );
+
+    transmit( block, 0 );
+
+    release( block );
+  }
+
+  /*
   if( m_freeze_active )
   {
     if( !m_paused )
@@ -297,6 +298,7 @@ void AUDIO_FREEZE_EFFECT::update()
       release( block );
     }
   }
+  */
 }
 
 void AUDIO_FREEZE_EFFECT::set_bit_depth_impl( int sample_size_in_bits )
@@ -304,11 +306,9 @@ void AUDIO_FREEZE_EFFECT::set_bit_depth_impl( int sample_size_in_bits )
   if( sample_size_in_bits != m_sample_size_in_bits )
   {
       m_sample_size_in_bits       = sample_size_in_bits;
-      m_buffer_size_in_samples    = freeze_queue_size_in_samples( m_sample_size_in_bits );
+      m_buffer_size_in_samples    = delay_buffer_size_in_samples( m_sample_size_in_bits );
       
       m_head                      = 0.0f;
-      m_loop_start                = 0;
-      m_loop_end                  = 0;
     
       memset( m_buffer, 0, sizeof(m_buffer) );
 
@@ -320,138 +320,8 @@ void AUDIO_FREEZE_EFFECT::set_bit_depth_impl( int sample_size_in_bits )
   }
 }
 
-void AUDIO_FREEZE_EFFECT::set_length_impl( float length )
-{  
-  ASSERT_MSG( m_loop_start >= 0 && m_loop_start < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_length_impl() pre" );
-  ASSERT_MSG( m_loop_end >= 0 && m_loop_end < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_length_impl() pre" ); 
-  ASSERT_MSG( length >= 0 && length <= 1.0f, "AUDIO_FREEZE_EFFECT::set_length_impl()" );
-
-  const int loop_length = roundf( length * m_buffer_size_in_samples );
-  m_loop_end            = min( m_loop_start + loop_length, m_buffer_size_in_samples - 1 );
-
-#ifdef DEBUG_OUTPUT
-  if( m_loop_end < m_loop_start || m_loop_start < 0 || m_loop_start > m_buffer_size_in_samples - 1 || m_loop_end < 0 || m_loop_end > m_buffer_size_in_samples - 1 )
-  {
-    Serial.print( "set length** loop_start:" );
-    Serial.print( m_loop_start );
-    Serial.print( " loop_end:" );
-    Serial.print( m_loop_end );
-    Serial.print( " loop_length:" );
-    Serial.print( loop_length );
-    Serial.print( " length:" );
-    Serial.print( length );
-    Serial.print( " buffer size:");
-    Serial.print( m_buffer_size_in_samples );
-    Serial.print( "\n" );
-  }
-#endif
-
-  ASSERT_MSG( m_loop_end >= m_loop_start, "AUDIO_FREEZE_EFFECT::set_length_impl()" );
-  ASSERT_MSG( m_loop_start >= 0 && m_loop_start < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_length_impl() post" );
-  ASSERT_MSG( m_loop_end >= 0 && m_loop_end < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_length_impl() post" );
-}
-
-void AUDIO_FREEZE_EFFECT::set_speed_impl( float speed )
-{
-  if( speed < 0.5f )
-  {
-    // put in the range 0..1 
-    float r = speed * 2.0f;
-    m_speed = lerp( MIN_SPEED, 1.0f, r ); 
-  }
-  else
-  {
-    // put in the range 0..1
-    float r = ( speed - 0.5f ) * 2.0f;
-    m_speed = lerp( 1.0f, MAX_SPEED, r );    
-  }
-}
-
-void AUDIO_FREEZE_EFFECT::set_centre_impl( float centre )
-{
-  ASSERT_MSG( centre >= 0 && centre < 1.0f, "AUDIO_FREEZE_EFFECT::set_centre_impl()" );
-  
-  int centre_index      = roundf( centre * m_buffer_size_in_samples );
-
-  int loop_length       = min( m_loop_end - m_loop_start + 1, m_buffer_size_in_samples - 1 );
-  ASSERT_MSG( loop_length < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_centre() loop too long" );
-  int half_loop_length  = loop_length / 2;
-  m_loop_start          = centre_index - half_loop_length;
-  m_loop_end            = m_loop_start + loop_length;
-
-  if( m_loop_start < 0 )
-  {
-    m_loop_start      = 0;
-    m_loop_end        = loop_length - 1;
-  }
-  else if( m_loop_end > m_buffer_size_in_samples - 1 )
-  {
-    m_loop_end        = m_buffer_size_in_samples - 1;
-    m_loop_start      = m_loop_end - loop_length - 1;
-  }
-
-  // not sure why these clamps are required, the code above should ensure they are correct, but it asserts without it - investigate
-  m_loop_start        = max( m_loop_start, 0 );
-  m_loop_end          = min( m_loop_end, m_buffer_size_in_samples - 1 );
-
-#ifdef DEBUG_OUTPUT
-  if( m_loop_end < m_loop_start || m_loop_start < 0 || m_loop_start > m_buffer_size_in_samples - 1 || m_loop_end < 0 || m_loop_end > m_buffer_size_in_samples - 1 )
-  {
-    Serial.print( "set set_centre_impl*** loop_start:" );
-    Serial.print( m_loop_start );
-    Serial.print( " loop_end:" );
-    Serial.print( m_loop_end );
-    Serial.print( " loop_length:" );
-    Serial.print( loop_length );
-    Serial.print( " buffer size:");
-    Serial.print( m_buffer_size_in_samples );
-    Serial.print( " centre:" );
-    Serial.print( centre_index );
-    Serial.print( "\n" );
-  }
-#endif
-
-  ASSERT_MSG( m_loop_end >= m_loop_start, "AUDIO_FREEZE_EFFECT::set_centre()" );
-  ASSERT_MSG( m_loop_start >= 0 && m_loop_start < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_centre()" );
-  ASSERT_MSG( m_loop_end >= 0 && m_loop_end < m_buffer_size_in_samples, "AUDIO_FREEZE_EFFECT::set_centre()" );
-}
-
-bool AUDIO_FREEZE_EFFECT::is_freeze_active() const
-{
-  return m_freeze_active; 
-}
-
-void AUDIO_FREEZE_EFFECT::set_freeze( bool active )
-{
-  m_freeze_active = active;  
-}
-
-void AUDIO_FREEZE_EFFECT::set_reverse( bool reverse )
-{
-  m_reverse = reverse;
-}
-
-void AUDIO_FREEZE_EFFECT::set_length( float length )
-{
-  m_next_length = length;
-  //set_length_impl(length);
-}
-
-void AUDIO_FREEZE_EFFECT::set_centre( float centre )
-{
-  m_next_centre = centre;
-  //set_centre_impl(centre);
-}
-
-void AUDIO_FREEZE_EFFECT::set_speed( float speed )
-{
-  m_next_speed = speed;
-  //set_speed_impl(speed);
-}
-
 void AUDIO_FREEZE_EFFECT::set_bit_depth( int sample_size_in_bits )
 {
   m_next_sample_size_in_bits = sample_size_in_bits;
   //set_bit_depth_impl( sample_size_in_bits );
 }
-*/
